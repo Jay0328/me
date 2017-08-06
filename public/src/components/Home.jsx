18884a96a8e60ad7b742e8316a8c7713870fff70
Taku <gcobs135102@gmail.com>
2017-08-06 11:52:43 +0800

    page menu

--- public/src/components/Home.jsx
+++ public/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor(props) {
@@ -10,13 +11,58 @@ class Home extends React.Component {
     this.renderPageMenu = this.renderPageMenu.bind(this);
   }
 
-  renderPageMenu() {
-    const { page, totalPage } = this.props;
-    if (totalPage <= 5) {
-      return null;
+  componentWillUpdate(nextProps) {
+    if (this.props.match.params.page !== nextProps.match.params.page) {
+      this.props.fetchArticlesList(parseInt(nextProps.match.params.page, 10));
     }
-    console.log([...Array(totalPage).keys()], page, totalPage);
-    return <div>qwer</div>;
+  }
+
+  renderPageMenu() {
+    let menu = [];
+    const { page, totalPage } = this.props;
+    let menuStart = page - 2;
+    let menuEnd;
+    while (menuStart < 1) menuStart += 1;
+    menuEnd = menuStart + 4;
+    while (menuEnd > totalPage) {
+      menuEnd -= 1;
+      if (menuStart > 1) menuStart -= 1;
+    }
+    /* prev */
+    if (page > 1) {
+      menu = [
+        ...menu,
+        <Link to={page > 2 ? `/page/${page - 1}` : '/'} key={`link-page-${page - 1}`}>&#x21E6;</Link>
+      ];
+    }
+    if (menuStart > 1) {
+      menu = [
+        ...menu,
+        <Link to='/' key='link-page-1'>1</Link>,
+        <div key='page-divider-left'>...</div>
+      ];
+    }
+    /* page menu */
+    menu = [
+      ...menu,
+      Array.from({ length: (menuEnd - menuStart) + 1 }, (v, k) => k + menuStart)
+        .map(p => {
+          if (p === page) return <div key={`link-page-${p}`}>{p}</div>;
+          return <Link to={p !== 1 ? `/page/${p}` : '/'} key={`link-page-${p}`}>{p}</Link>;
+        })
+    ];
+    /* next */
+    if (menuEnd < totalPage) {
+      menu = [
+        ...menu,
+        <div key='page-divider-right'>...</div>,
+        <Link to={`/page/${totalPage}`} key={`link-page-${totalPage}`}>{totalPage}</Link>
+      ];
+    }
+    if (page < totalPage) {
+      menu = [...menu, <Link to={`/page/${page + 1}`} key={`link-page-${page + 1}`}>&#x21E8;</Link>];
+    }
+    return <div>{menu}</div>;
   }
 
   render() {
@@ -33,9 +79,7 @@ class Home extends React.Component {
             );
           })}
         </div>
-        <div>
-          {this.renderPageMenu()}
-        </div>
+        {this.renderPageMenu()}
       </div>
     );
   }
