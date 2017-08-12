dbb1b8807fb72736b0408bec60ebca5889c67567
Taku <gcobs135102@gmail.com>
2017-08-13 02:59:04 +0800

    footer css

--- public/src/components/Footer.jsx
+++ public/src/components/Footer.jsx
@@ -6,13 +6,54 @@ import { facebook, github } from '../../config';
 const Footer = ({ isAuth, logout }) => {
   return (
     <footer>
-      <div className="">
-      </div>
+      <p className="copyright">
+        <div className="visitor">
+          <span>
+            本站總訪問量
+          </span>
+          <span>? 次</span>
+          <span>
+            本站訪客數
+          </span>
+          <span>? 次</span>
+        </div>
+        <span>
+          轉載請 CC 內容作者註明
+        </span>
+        <span>
+          Copyright © Taku 2017
+        </span>
+        <span>
+          Icon by&nbsp;
+          <i className="fa fa-font-awesome" aria-hidden="true"></i>&nbsp;
+          <a target="_blank" rel="external nofollow noopener noreferrer" href="http://fontawesome.io/">
+            Font Awesome
+          </a>
+        </span>
+        <span>
+          Theme by&nbsp;
+          <a target="_blank" rel="external nofollow noopener noreferrer" href="https://calpa.me">
+            Calpa Liu
+          </a>
+          {', '}Modified by&nbsp;
+          <a href="/">Me</a>
+        </span>
+      </p>
       <div className="contact-info">
-        <a className="contact-info-icon" target="_blank" href={facebook}>
+        <a
+          className="contact-info-icon"
+          target="_blank"
+          rel="external nofollow noopener noreferrer"
+          href={facebook}
+        >
           <i className="fa fa-facebook-square" aria-hidden="true"></i>
         </a>
-        <a className="contact-info-icon" target="_blank" href={github}>
+        <a
+          className="contact-info-icon"
+          target="_blank"
+          rel="external nofollow noopener noreferrer"
+          href={github}
+        >
           <i className="fa fa-github" aria-hidden="true"></i>
         </a>
       </div>
