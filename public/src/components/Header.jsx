263d01ee3ce519dad90027ef5cbf0384e668a5ab
Taku <gcobs135102@gmail.com>
2017-08-13 23:45:34 +0800

    header: about me

--- public/src/components/Header.jsx
+++ public/src/components/Header.jsx
@@ -4,6 +4,11 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
+      <div className="about-me">
+        <img src="/images/profile.png" alt="me" />
+        <h4>Taku</h4>
+        <p>人生就是&nbsp;!#$(%^(@#^$*#@!$^&@$!(</p>
+      </div>
     </header>
   );
 };
