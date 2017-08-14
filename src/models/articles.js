b9905b6c5e3ddf46f8de430ad3ce507b23fd404a
Taku <gcobs135102@gmail.com>
2017-08-14 23:21:35 +0800

    ident by spaces 2

--- src/models/articles.js
+++ src/models/articles.js
@@ -2,35 +2,35 @@ const mongoose = require('mongoose');
 const Schema = require('mongoose').Schema;
 
 const articlesSchema = new Schema({
-    year: {
-        type: String,
-        required: true,
-    },
-    month: {
-        type: String,
-        required: true,
-    },
-    day: {
-        type: String,
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    url: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    tags: {
-        type: [String],
-        required: true
-    },
-    intro: String
+  year: {
+    type: String,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+  day: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true
+  },
+  intro: String
 });
 
 const Articles = mongoose.model('Articles', articlesSchema, 'Articles');
