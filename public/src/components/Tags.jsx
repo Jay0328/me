7c498a6c09c536391f2968b12b7900e349d3a630
Taku <gcobs135102@gmail.com>
2017-08-30 03:04:53 +0800

    revise tags.jsx render

--- public/src/components/Tags.jsx
+++ public/src/components/Tags.jsx
@@ -15,32 +15,40 @@ class Tags extends React.PureComponent {
     const tagFilter = this.props.match.params.tag;
     const { tags } = this.props;
 
+    const tagsCloud = (
+      <div className="tags-labels">
+        {Object.keys(tags).map(tagName => (
+          <TagLabel key={tagName} mode={'cloud'} tagName={tagName} articleNum={tags[tagName].size} />
+        ))}
+      </div>
+    );
+
+    const tagsLists = (
+      <div className="tags-lists">
+        {Object.keys(tags).filter(tagName => !tagFilter || tagFilter === tagName.replace(' ', ''))
+          .map(tagName => (
+            <div id={tagName.replace(' ', '')} className="tag-list" key={tagName}>
+              <TagLabel mode={'label'} tagName={tagName} />
+              {tags[tagName].map(({ year, month, day, title, url }) => (
+                <div className="article" key={`/${year}/${month}/${day}/${url}`}>
+                  <Link to={`/${year}/${month}/${day}/${url}`}>
+                    <h3 className="article-title">{title}</h3>
+                  </Link>
+                  <hr />
+                </div>
+              ))}
+            </div>
+          ))}
+      </div>
+    );
+
     return (
       <section className="tags">
         <Header />
         <div className="container">
           <div className="tags-container">
-            <div className="tags-labels">
-              {Object.keys(tags).map(tagName => (
-                <TagLabel key={tagName} mode={'cloud'} tagName={tagName} articleNum={tags[tagName].size} />
-              ))}
-            </div>
-            <div className="tags-lists">
-              {Object.keys(tags).filter(tagName => !tagFilter || tagFilter === tagName.replace(' ', ''))
-                .map(tagName => (
-                  <div id={tagName.replace(' ', '')} className="tag-list" key={tagName}>
-                    <TagLabel mode={'label'} tagName={tagName} />
-                    {tags[tagName].map(({ year, month, day, title, url }) => (
-                      <div className="article" key={`/${year}/${month}/${day}/${url}`}>
-                        <Link to={`/${year}/${month}/${day}/${url}`}>
-                          <h3 className="article-title">{title}</h3>
-                        </Link>
-                        <hr />
-                      </div>
-                    ))}
-                  </div>
-                ))}
-            </div>
+            {tagsCloud}
+            {tagsLists}
           </div>
           <Profile />
         </div>
